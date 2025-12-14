b3c814079c15d87136a561820bb0b8fb2ac0d74f
mahima07 <99538538+mahima0711@users.noreply.github.com>
2025-12-14 11:09:59 +0530

    Time: 8 ms (88.42%), Space: 48.9 MB (46.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,30 +1,65 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
+
         if(intervals.length == 0)
         {
             return new int[0][0];
         }
         Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
-
         List<int[]> merged = new ArrayList<>();
-        merged.add(intervals[0]);
+
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
 
         for(int i=1;i<intervals.length;i++)
         {
-            int last[] = merged.get(merged.size()-1);
-            int curr[] = intervals[i];
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
 
-            if(last[1] >= curr[0])
+            if(end1 >= start2)
             {
-                last[1] = Math.max(last[1],curr[1]);
+                start1 = start1;
+                end1 = Math.max(end1, end2);
+                continue;
             }
-            else
-            {
-                merged.add(curr);
-            }
+             
+             int[] arr={start1,end1};
+            merged.add(arr);
+
+            start1 = start2;
+            end1 = end2;
         }
 
-        return merged.toArray(new int [merged.size()][]);
+        int[] arr={start1,end1};
+            merged.add(arr);
+        return merged.toArray(new int[merged.size()][]);
+
+        // if(intervals.length == 0)
+        // {
+        //     return new int[0][0];
+        // }
+        // Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+
+        // List<int[]> merged = new ArrayList<>();
+        // merged.add(intervals[0]);
+
+        // for(int i=1;i<intervals.length;i++)
+        // {
+        //     int last[] = merged.get(merged.size()-1);
+        //     int curr[] = intervals[i];
+
+        //     if(last[1] >= curr[0])
+        //     {
+        //         last[1] = Math.max(last[1],curr[1]);
+        //     }
+        //     else
+        //     {
+        //         merged.add(curr);
+        //     }
+        // }
+
+        // return merged.toArray(new int [merged.size()][]);
+
+
     }
 }

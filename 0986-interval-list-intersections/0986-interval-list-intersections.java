41ad779811496e68e656a3bc8b26a85c94c77787
mahima07 <99538538+mahima0711@users.noreply.github.com>
2025-12-14 16:30:36 +0530

    Time: 3 ms (98.53%), Space: 47.5 MB (7.44%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,52 +1,37 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
 
-        int i = 0;
+       int i = 0;
         int j = 0;
-        int start1 = 0;
-        int end1 = 0;
 
         List<int[]> list = new ArrayList<>();
-        if (firstList.length == 0 || secondList.length ==0)
-            return new int[0][0];
 
-        // if (secondList.length == 0)
-        //     return secondList;
+        if (firstList.length == 0 || secondList.length == 0) {
+            return new int[0][0];
+        }
 
         while (i < firstList.length && j < secondList.length) {
+
+            int start1 = firstList[i][0];
+            int end1 = firstList[i][1];
+
             int start2 = secondList[j][0];
             int end2 = secondList[j][1];
-            start1 = firstList[i][0];
-            end1 = firstList[i][1];
 
-            if (start1 <= start2) {
+            // Intersection check using a single formula
+            int s = Math.max(start1, start2);
+            int e = Math.min(end1, end2);
 
-                if (end1 >= start2) {
-                    int s = Math.max(start1,start2);
-                    int e =Math.min(end1, end2);
-
-                    list.add(new int[] { s, e});
-                }
-            }
-            else
-            {
-                if(end2 >= start1)
-                {
-                    int s = Math.max(start1,start2);
-                    int e =Math.min(end1, end2);
-
-                    list.add(new int[] { s, e});
-
-                }
-
+            if (s <= e) {
+                list.add(new int[]{s, e});
             }
 
-            if(end1 <= end2)
-            {
+            // Move the pointer for the interval that ends first
+            if (end1 < end2) {
                 i++;
-            }
-            else
+            } else {
                 j++;
+            }
         }
 
         return list.toArray(new int[list.size()][]);

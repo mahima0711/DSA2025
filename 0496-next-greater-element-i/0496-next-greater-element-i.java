515d7fa2d338fff9e2324375740e1abb3147caca
mahima07 <99538538+mahima0711@users.noreply.github.com>
2025-12-16 16:24:12 +0530

    Time: 4 ms (62.63%), Space: 45 MB (51.17%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
         Stack<Integer> stack = new Stack<>();
-        int ans[] = new int[nums2.length];
+        Map<Integer, Integer> map = new HashMap<>();
         int res[] = new int[nums1.length];
         stack.push(nums2[nums2.length - 1]);
-        ans[nums2.length-1] = -1;
+        map.put(nums2[nums2.length-1], -1);
 
         for (int i = nums2.length-2; i >= 0; i--) {
             while (!stack.empty() && stack.peek() <= nums2[i]) {
@@ -13,21 +13,22 @@ class Solution {
             }
 
             if (stack.empty()) {
-                ans[i] = -1;
+                map.put(nums2[i],-1);
             } else {
-                ans[i] = stack.peek();
+                map.put(nums2[i],stack.peek());
             }
             stack.push(nums2[i]);
         }
-        int i = 0;
-        while (i < nums1.length) {
-            int j = 0;
-            while (nums1[i] != nums2[j]) {
-                j++;
+         
+         for(int i=0;i< nums1.length;i++)
+         {
+            if(map.containsKey(nums1[i]))
+            {
+                res[i] = map.get(nums1[i]);
             }
-            res[i] = ans[j];
-            i++;
-        }
+         }
+
+       
         return res;
 
     }

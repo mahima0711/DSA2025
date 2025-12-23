1e43a39381162222735f9dd9aeddc50632a73407
mahima07 <99538538+mahima0711@users.noreply.github.com>
2025-12-23 19:52:17 +0530

    Time: 30 ms (12.39%), Space: 47.3 MB (9.01%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,9 +2,11 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
         Arrays.sort(nums);
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
             for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
                 int left = j + 1;
                 int right = nums.length - 1;
                 while (left < right) {
@@ -16,9 +18,13 @@ class Solution {
                         list.add(nums[left]);
                         list.add(nums[right]);
                         ans.add(list);
-                    }
+                    left++;
+                    right--;
 
-                    if(sum < target)
+                    while(left< right && nums[left] == nums[left-1]) left++;
+                    while(left< right && nums[right] == nums[right+1]) right--;
+                    }
+                    else if (sum < target)
                     left++;
                     else
                     right--;

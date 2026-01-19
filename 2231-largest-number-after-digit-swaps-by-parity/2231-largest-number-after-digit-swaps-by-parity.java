ba0ccf13ed5613600a9aac94ed98f165614185cc
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-01-19 22:43:46 +0530

    Time: 2 ms (76.76%), Space: 42.5 MB (44.31%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,26 +1,33 @@
 class Solution {
     public int largestInteger(int num) {
 
+        PriorityQueue<Integer> oddPq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> evenPq = new PriorityQueue<>(Collections.reverseOrder());
+
         char[] arr = Integer.toString(num).toCharArray();
 
         for (int i = 0; i < arr.length; i++) {
-            int k = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if ((arr[i] - '0') % 2 == (arr[j] - '0') % 2) {
-                    if ((arr[k]) < (arr[j])) {
-                        k = j;
-                    }
-                }
+            if ((arr[i] - '0') % 2 == 0) {
+                evenPq.add(arr[i]-'0');
+            } else {
+                oddPq.add(arr[i]-'0');
             }
-
-            if (k != i) {
-                char temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
-            }
-
         }
 
-        return Integer.parseInt(new String(arr));
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if((arr[i]-'0') % 2 == 0)
+            {
+                sb.append(evenPq.poll());
+            }
+            else
+            {
+                sb.append(oddPq.poll());
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
     }
 }

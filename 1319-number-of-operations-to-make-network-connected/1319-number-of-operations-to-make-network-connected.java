8a84487ccefa1bb49ae890877d467b285396b693
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-03-17 00:05:27 +0530

    Time: 4 ms (90.59%), Space: 66.1 MB (76.03%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,69 +1,57 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        
+
         int parent[] = new int[n];
         int rank[] = new int[n];
 
-        if(connections.length < n-1)
-        {
+        if (connections.length < n - 1) {
             return -1;
         }
 
-        for(int i=0; i<n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
 
-        int components = n;
+        int ans = n;
 
-        for(int c[] : connections)
-        {
+        for (int c[] : connections) {
             int u = c[0];
             int v = c[1];
 
             int up = find(u, parent);
             int vp = find(v, parent);
 
-             if(up != vp) {
-                union(up, vp, parent, rank);
-                components--;
+            if (up != vp) {
+                ans--;
+                union(u, v, parent, rank);
             }
 
         }
 
-        return components -1;
+        return ans - 1;
     }
 
-    int find(int i,int[] parent)
-    {
-        if(parent[i] == i)
-        {
+    int find(int i, int[] parent) {
+        if (parent[i] == i) {
             return i;
         }
 
         return parent[i] = find(parent[i], parent);
     }
 
-    void union(int u, int v, int[] parent, int[] rank)
-    {
+    void union(int u, int v, int[] parent, int[] rank) {
         int up = find(u, parent);
         int vp = find(v, parent);
 
-        if(up == vp)
-        {
+        if (up == vp) {
             return;
         }
 
-        if(rank[up] > rank[vp])
-        {
+        if (rank[up] > rank[vp]) {
             parent[vp] = up;
-        }
-        else if(rank[up] < rank[vp])
-        {
+        } else if (rank[up] < rank[vp]) {
             parent[up] = vp;
-        }
-        else
-        {
+        } else {
             parent[up] = vp;
             rank[vp]++;
         }

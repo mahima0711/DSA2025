a4c88b9dac69e264f58536af272e02bdb2650997
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-03-17 00:03:59 +0530

    Time: 4 ms (90.59%), Space: 65.8 MB (84.53%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,71 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        
+        int parent[] = new int[n];
+        int rank[] = new int[n];
+
+        if(connections.length < n-1)
+        {
+            return -1;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            parent[i] = i;
+        }
+
+        int components = n;
+
+        for(int c[] : connections)
+        {
+            int u = c[0];
+            int v = c[1];
+
+            int up = find(u, parent);
+            int vp = find(v, parent);
+
+             if(up != vp) {
+                union(up, vp, parent, rank);
+                components--;
+            }
+
+        }
+
+        return components -1;
+    }
+
+    int find(int i,int[] parent)
+    {
+        if(parent[i] == i)
+        {
+            return i;
+        }
+
+        return parent[i] = find(parent[i], parent);
+    }
+
+    void union(int u, int v, int[] parent, int[] rank)
+    {
+        int up = find(u, parent);
+        int vp = find(v, parent);
+
+        if(up == vp)
+        {
+            return;
+        }
+
+        if(rank[up] > rank[vp])
+        {
+            parent[vp] = up;
+        }
+        else if(rank[up] < rank[vp])
+        {
+            parent[up] = vp;
+        }
+        else
+        {
+            parent[up] = vp;
+            rank[vp]++;
+        }
+    }
+}

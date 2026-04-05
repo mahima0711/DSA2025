c95b1b193ea3027b859d7bb26e5e0fb1f1492d65
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-04-05 12:15:20 +0530

    Time: 3 ms (75.61%), Space: 53.5 MB (85.89%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -4,17 +4,9 @@ class Solution {
         int[] outDegree = new int[n + 1];
         int[] inDegree = new int[n+1];
 
-        List<List<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
-        }
-
         for (int i = 0; i < trust.length; i++) {
             int u = trust[i][0];
             int v = trust[i][1];
-
-            adj.get(u).add(v);
 
             outDegree[u]++;
 

e2600aa48a9c96996d5606c5b9027afd0a17b2cc
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-04-07 01:09:37 +0530

    Time: 0 ms (100%), Space: 42.3 MB (9.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,9 +8,8 @@ class Solution {
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
-        dp[3] = 3;
 
-        for(int i=4; i<=n ;i++)
+        for(int i=3; i<=n ;i++)
         {
             dp[i] = dp[i-1] + dp[i-2];
         }

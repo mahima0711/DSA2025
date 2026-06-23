fb52207388b9104214211292b4e0b2e85026d32f
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-06-23 23:39:10 +0530

    Time: 26 ms (20.03%), Space: 54.1 MB (23.05%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -9,12 +9,14 @@ class Solution {
 
         int dp[][] = new int[m + 1][n + 1];
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = i + j;
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
@@ -27,7 +29,7 @@ class Solution {
                 result.append(str1.charAt(i - 1));
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            } else if (dp[i - 1][j] < dp[i][j - 1]) {
                 result.append(str1.charAt(i - 1));
                 i--;
             } else {

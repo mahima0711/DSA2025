387409008955de4753fa1da8202db1c99c06077d
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-06-24 00:16:44 +0530

    Time: 116 ms (20.88%), Space: 242.9 MB (5.05%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,9 +1,18 @@
 class Solution {
+
+    int dp[][];
     public int countSubstrings(String s) {
 
         int n = s.length();
 
         int count = 0;
+
+        dp = new int[1001][1001];
+
+        for(int i=0;i<1001;i++)
+        {
+            Arrays.fill(dp[i], -1);
+        }
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
@@ -21,10 +30,17 @@ class Solution {
             return true;
         }
 
-        if (s.charAt(i) == s.charAt(j)) {
-            return check(i + 1, j - 1, s);
+        if(dp[i][j] != -1)
+        {
+            return dp[i][j] == 1;
         }
 
+          if (s.charAt(i) == s.charAt(j) && check(i + 1, j - 1, s)) {
+            dp[i][j] = 1;
+            return true;
+        }
+
+        dp[i][j] = 0;
         return false;
     }
 }

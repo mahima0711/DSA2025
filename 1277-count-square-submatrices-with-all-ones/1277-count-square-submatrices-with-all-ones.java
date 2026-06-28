e3071bb2e0b829f52ebc720b0f35334a061c726a
mahima07 <99538538+mahima0711@users.noreply.github.com>
2026-06-28 23:42:19 +0530

    Time: 7 ms (53.05%), Space: 61.5 MB (89.3%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -9,52 +9,24 @@ class Solution {
 
         dp = new int[m][n];
 
-        for(int i=0;i<m;i++)
-        {
-            Arrays.fill(dp[i], -1);
-        }
+        int result = 0;
 
-        int count =0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j] == 1)
-                {
-                    count += solve(matrix, i, j, m, n);
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else {
+                    if (matrix[i][j] == 1) {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
+                    }
                 }
+
+                result += dp[i][j];
             }
         }
 
-        return count;
+        return result;
     }
 
-    int solve(int[][] arr, int i, int j, int m, int n)
-    {
-        if(i >= m || j>=n)
-        {
-            return 0;
-        }
-
-        if(arr[i][j] == 0)
-        {
-            return 0;
-        }
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        int right = solve( arr, i, j+1, m,n);
-
-        int dia = solve(arr, i+1, j+1, m, n);
-        int down = solve(arr, i+1, j, m, n);
-
-        int result = 1 + Math.min(right, Math.min(dia, down));
-
-        return dp[i][j] = result;
-
-    }
 }
